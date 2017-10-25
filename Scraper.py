a3bd7eb50d71d68d72031312c5dc5373a45768aa
matt.grant <nzmattgrant@gmail.com>
2017-10-25 16:42:37 +1300

    Parsed the description text into html to clear out comments. Included the chrome driver exe.

--- Scraper.py
+++ Scraper.py
@@ -2,6 +2,7 @@ import requests
 import csv
 from selenium import webdriver
 from bs4 import BeautifulSoup,NavigableString,Comment
+from html.parser import HTMLParser
 
 site_root = "https://www.seek.co.nz"
 
@@ -9,7 +10,7 @@ current_page = ""
 next_page = site_root + "/jobs-in-information-communication-technology"
 job_links = []
 current_page_number = 0
-while current_page != next_page and len(job_links) <= 100:
+while current_page != next_page and len(job_links) <= 1:
     current_page = next_page
     current_page_number = current_page_number + 1
     current_request = requests.get(current_page)
@@ -29,13 +30,17 @@ with open("first100.csv", "w+") as csvfile:
     fieldnames = ['description']
     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
     writer.writeheader()
-    for link in job_links[0:5]:
-        driver = webdriver.Chrome("C:\Program Files\ChromeDriver\chromedriver.exe")
+    for link in job_links[0:1]:
+        driver = webdriver.Chrome("chromedriver.exe")
         driver.get(site_root + link)
         page_soup = BeautifulSoup(driver.page_source, "html.parser")
         description = page_soup.find("div", attrs={"data-automation": "jobDescription"})
         if description is not None:
             description_text = ""
-            for child in [c for c in BeautifulSoup(description.text, "lxml").recursiveChildGenerator() if type(c) == NavigableString and type(c) != Comment]:
-                description_text = description_text + "; " + child
+            for child in [c for c in BeautifulSoup(HTMLParser().unescape(description.text), "html.parser").recursiveChildGenerator() if type(c) == NavigableString]:
+                if isinstance(child, Comment) or child.isspace() or child == "":
+                    continue
+                print("child = " + child)
+                description_text = description_text + "; " + child.strip()
             writer.writerow({"description": description_text})
+
