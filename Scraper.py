c85e930c3a258a23abb25c9e733a7ddbff06b5d3
Matt <nzmattgrant@gmail.com>
2017-11-21 19:56:30 +1300

    added keyword extraction

--- Scraper.py
+++ Scraper.py
@@ -3,14 +3,16 @@ import csv
 from selenium import webdriver
 from bs4 import BeautifulSoup,NavigableString,Comment
 from html.parser import HTMLParser
+from rake_nltk import Rake
 
 site_root = "https://www.seek.co.nz"
+r = Rake()
 
 current_page = ""
 next_page = site_root + "/jobs-in-information-communication-technology"
 job_links = []
 current_page_number = 0
-while current_page != next_page and len(job_links) <= 100:
+while current_page != next_page and len(job_links) <= 10:
     current_page = next_page
     current_page_number = current_page_number + 1
     current_request = requests.get(current_page)
@@ -26,11 +28,11 @@ while current_page != next_page and len(job_links) <= 100:
     print("current page: " + current_page)
     print("next page: " + next_page)
 print(job_links)
-with open("first100.csv", "w+", encoding='utf-8') as csvfile:
-    fieldnames = ['title', 'description', 'advertiser-name', 'date', 'work-type']
+with open("first10WithKeyWords.csv", "w+", encoding='utf-8') as csvfile:
+    fieldnames = ['title', 'description', 'advertiser-name', 'date', 'work-type', "keywords"]
     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
     writer.writeheader()
-    for link in job_links[0:100]:
+    for link in job_links[0:5]:
         driver = webdriver.Chrome("chromedriver.exe")
         driver.get(site_root + link)
         page_soup = BeautifulSoup(driver.page_source, "html.parser")
@@ -48,6 +50,9 @@ with open("first100.csv", "w+", encoding='utf-8') as csvfile:
                 print("child = " + child)
                 description_text = description_text + " " + child.strip()
             row_object.update({"description": description_text})
+            r.extract_keywords_from_text(description_text)
+            keywords = r.get_ranked_phrases()
+            row_object.update({"keywords": keywords})
 
         advertiser_name = page_soup.find(attrs={"data-automation": "job-details-header-advertiser-name"})
         if advertiser_name is not None:
